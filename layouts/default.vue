482913c53dbfe4ac5c1c5034016fbf5b7f668721
tatsuru koike <t-koike@marsflag.com>
2025-05-07 11:33:45 +0900

    feat: refine layout styling; remove minimum height constraints for main content area

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@ import AppHeader from "@/components/layout/AppHeader.vue";
     <AppHeader />
 
     <main
-      class="mx-auto w-full mt-[53px] md:mt-[68px] max-w-[1440px] px-4 py-8 md:p-8 md:min-h-[calc(100vh-68px)] min-h-[calc(100vh-53px)]"
+      class="mx-auto w-full mt-[53px] md:mt-[68px] max-w-[1440px] px-4 py-8 md:p-8"
     >
       <NuxtPage />
     </main>

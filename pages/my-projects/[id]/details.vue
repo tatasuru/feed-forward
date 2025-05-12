7574317fedafb433b7937db6a38ea7fdd9adbe37
tatsuru koike <t-koike@marsflag.com>
2025-05-12 16:03:33 +0900

    feat: open project owner's website in a new tab

--- pages/my-projects/[id]/details.vue
+++ pages/my-projects/[id]/details.vue
@@ -419,6 +419,7 @@ async function deleteProject() {
                 <NuxtLink
                   v-if="projectWithFeedback.owner.website"
                   :to="projectWithFeedback.owner.website"
+                  target="_blank"
                   class="text-sm text-link flex items-center gap-1"
                 >
                   <Icon name="mdi:link" class="!size-4 text-link" />

11fed5e1698a792b9b30eabd4bb9e8a3db384515
tatsuru koike <t-koike@marsflag.com>
2025-06-16 20:01:58 +0900

    feat: refactor form template handling; implement dynamic template fetching and enhance rating criteria management

--- pages/create-form/index.vue
+++ pages/create-form/index.vue
@@ -1,35 +1,37 @@
 <script setup lang="ts">
+import { ref, computed } from "vue";
+
+const supabase = useSupabaseClient();
+
+type FormTemplate = {
+  id: number;
+  category: string;
+  description: string;
+  is_featured: boolean;
+  name: string;
+  item_count: number;
+  preview_items: {
+    name: string;
+    description: string;
+    display_order: number;
+    rating_code: string;
+    rating_type: string;
+  }[];
+};
+
+type RatingPerCriteria = {
+  name: string;
+  description: string;
+  display_order: number;
+  rating_code: string;
+  rating_type: string;
+  value?: number;
+};
+
 /********************************
  * for forms preview
  ********************************/
-const templates = [
-  {
-    image: "/feedback-thumbnail.png",
-    icon: "mdi:star-check",
-    value: "starFeedback",
-    label: "製品フィードバック",
-    description:
-      "5つの星で評価し、コメントを追加するフィードバックテンプレートです。",
-  },
-];
-
-const ratingPerCriteria = [
-  {
-    title: "使いやすさ",
-    description: "アプリケーションの使いやすさを評価してください",
-    value: 0,
-  },
-  {
-    title: "機能性",
-    description: "アプリケーションの機能性を評価してください",
-    value: 0,
-  },
-  {
-    title: "デザイン",
-    description: "アプリケーションのデザインを評価してください",
-    value: 0,
-  },
-];
+const ratingPerCriteria = ref<RatingPerCriteria[]>([]);
 
 const hoverStarIndexObj = ref<{
   [key: number]: number;
@@ -38,6 +40,65 @@ const hoverStarIndexObj = ref<{
   1: -1,
   2: -1,
 });
+
+function clickPreviewButton(id: number, e: Event) {
+  e.preventDefault();
+  e.stopPropagation();
+  hoverStarIndexObj.value = {
+    0: -1,
+    1: -1,
+    2: -1,
+  };
+
+  const template = newTemplate.value?.find((template) => template.id === id);
+  if (template) {
+    ratingPerCriteria.value = template.preview_items.map((item) => ({
+      name: item.name,
+      description: item.description,
+      display_order: item.display_order,
+      rating_code: item.rating_code,
+      rating_type: item.rating_type,
+      value: 0,
+    }));
+    console.log(
+      "Preview items for template ID",
+      id,
+      ":",
+      ratingPerCriteria.value
+    );
+  } else {
+    console.error("Template not found for ID:", id);
+    ratingPerCriteria.value = [];
+  }
+}
+
+/********************************
+ * for form templates
+ ********************************/
+const newTemplate = computed(() => {
+  return formTemplates.value;
+});
+const { data: formTemplates } = await useAsyncData(
+  "projectsList",
+  async () => {
+    try {
+      let { data: templates, error } = await supabase
+        .from("template_catalog")
+        .select("*")
+        .order("is_featured", { ascending: false });
+
+      if (error) throw new Error(error.message);
+      console.log("Fetched templates:", templates);
+      return templates as FormTemplate[];
+    } catch (error) {
+      console.error("Error fetching projects:", error);
+      return [];
+    }
+  },
+  {
+    server: true,
+  }
+);
 </script>
 
 <template>
@@ -49,18 +110,21 @@ const hoverStarIndexObj = ref<{
     />
 
     <div class="grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-6">
-      <Card v-for="template in templates" :key="template.value">
-        <div>
-          <NuxtImg
-            :src="template.image"
-            alt="フィードバックテンプレート"
-            class="w-full h-32 object-contain rounded-t-md"
-          />
-        </div>
+      <Card
+        v-for="template in newTemplate"
+        :key="template.id"
+        class="cursor-pointer hover:shadow-lg transition-shadow duration-200 grid grid-rows-subgrid row-span-3"
+      >
+        <NuxtImg
+          src="/feedback-thumbnail.png"
+          alt="フィードバックテンプレート"
+          class="w-full h-32 object-contain rounded-t-md"
+        />
+
         <CardHeader class="flex h-fit">
           <div class="flex flex-col gap-1">
             <CardTitle class="text-sm font-semibold">
-              {{ template.label }}
+              {{ template.name }}
             </CardTitle>
             <CardDescription class="text-xs text-muted-foreground">
               {{ template.description }}
@@ -75,68 +139,150 @@ const hoverStarIndexObj = ref<{
                 variant="link"
                 type="button"
                 class="p-0 size-fit text-xs text-purple hover:underline cursor-pointer"
-                @click="(e: Event) => {
-                                        e.preventDefault();
-                                        e.stopPropagation();
-                                      }"
+                @click="(e: Event) => clickPreviewButton(template.id, e)"
               >
                 プレビューを見る
               </Button>
             </DialogTrigger>
             <DialogContent class="sm:max-w-[700px]">
               <DialogHeader>
-                <DialogTitle>製品フィードバックテンプレート</DialogTitle>
+                <DialogTitle>{{ template.name }}</DialogTitle>
                 <DialogDescription>
-                  このテンプレートは、5つの星で評価し、コメントを追加するフィードバックテンプレートです。5段階評価、コメント、匿名でのフィードバックが可能です。
+                  {{ template.description }}
                 </DialogDescription>
               </DialogHeader>
 
-              <!-- template -->
               <div
                 class="flex flex-col gap-6 border p-6 rounded-md w-full max-w-[600px] mx-auto"
               >
-                <!-- rating -->
-                <div v-for="(criteria, criteriaIndex) in ratingPerCriteria">
-                  <p class="text-sm font-medium">
-                    {{ criteria.title }}
-                  </p>
-                  <span class="text-xs text-muted-foreground">
-                    {{ criteria.description }}
-                  </span>
-                  <div class="flex items-center justify-between">
-                    <div class="flex items-center gap-2">
-                      <Button
-                        v-for="(_, index) in [1, 2, 3, 4, 5]"
-                        :key="index"
-                        variant="ghost"
-                        type="button"
-                        size="icon"
-                        class="cursor-pointer rounded-full hover:bg-yellow-500/20 dark:hover:bg-yellow-500/20"
-                        @click="hoverStarIndexObj[criteriaIndex] = index"
-                      >
-                        <Icon
-                          v-if="hoverStarIndexObj[criteriaIndex] >= index"
-                          name="mdi:star"
-                          class="!size-6 text-yellow-500"
-                        />
-                        <Icon
-                          v-else
-                          name="mdi:star-outline"
-                          class="!size-6 text-muted-foreground hover:text-yellow-500"
-                        />
-                      </Button>
+                <div
+                  v-for="(criteria, criteriaIndex) in ratingPerCriteria"
+                  :key="criteriaIndex"
+                >
+                  <!-- for star -->
+                  <div
+                    v-if="criteria.rating_code === 'star'"
+                    class="flex flex-col gap-2"
+                  >
+                    <div class="flex flex-col gap-1">
+                      <p class="text-sm font-medium">
+                        {{ criteria.name }}
+                      </p>
+                      <span class="flex text-xs text-muted-foreground">
+                        {{ criteria.description }}
+                      </span>
                     </div>
-                    <span class="text-sm text-muted-foreground">
-                      {{
-                        hoverStarIndexObj[criteriaIndex] < 0
-                          ? 0
-                          : hoverStarIndexObj[criteriaIndex] + 1
-                      }}/5
-                    </span>
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-center gap-2">
+                        <Button
+                          v-for="(_, index) in [1, 2, 3, 4, 5]"
+                          :key="index"
+                          variant="ghost"
+                          type="button"
+                          size="icon"
+                          class="cursor-pointer rounded-full hover:bg-yellow-500/20 dark:hover:bg-yellow-500/20"
+                          @click="hoverStarIndexObj[criteriaIndex] = index"
+                        >
+                          <Icon
+                            v-if="hoverStarIndexObj[criteriaIndex] >= index"
+                            name="mdi:star"
+                            class="!size-6 text-yellow-500"
+                          />
+                          <Icon
+                            v-else
+                            name="mdi:star-outline"
+                            class="!size-6 text-muted-foreground hover:text-yellow-500"
+                          />
+                        </Button>
+                      </div>
+                      <span class="text-sm text-muted-foreground">
+                        {{
+                          hoverStarIndexObj[criteriaIndex] < 0
+                            ? 0
+                            : hoverStarIndexObj[criteriaIndex] + 1
+                        }}/5
+                      </span>
+                    </div>
+                  </div>
+
+                  <!-- for scale_10 -->
+                  <div
+                    v-else-if="criteria.rating_code === 'scale_10'"
+                    class="flex flex-col gap-2"
+                  >
+                    <div class="flex flex-col gap-1">
+                      <p class="text-sm font-medium">
+                        {{ criteria.name }}
+                      </p>
+                      <span class="flex text-xs text-muted-foreground">
+                        {{ criteria.description }}
+                      </span>
+                    </div>
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-center gap-2">
+                        <Button
+                          v-for="index in 10"
+                          :key="index"
+                          variant="outline"
+                          type="button"
+                          size="icon"
+                          class="cursor-pointer"
+                          @click="hoverStarIndexObj[criteriaIndex] = index - 1"
+                        >
+                          {{ index }}
+                        </Button>
+                      </div>
+                      <span class="text-sm text-muted-foreground">
+                        {{
+                          hoverStarIndexObj[criteriaIndex] < 0
+                            ? 0
+                            : hoverStarIndexObj[criteriaIndex] + 1
+                        }}/10
+                      </span>
+                    </div>
+                  </div>
+
+                  <!-- for radio -->
+                  <div
+                    v-if="criteria.rating_code === 'radio'"
+                    class="flex flex-col gap-2"
+                  >
+                    <div class="flex flex-col gap-1">
+                      <p class="text-sm font-medium">
+                        {{ criteria.name }}
+                      </p>
+                      <span class="flex text-xs text-muted-foreground">
+                        {{ criteria.description }}
+                      </span>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                      <RadioGroup>
+                        <div
+                          v-for="(option, index) in [
+                            'はい',
+                            'いいえ',
+                            'どちらでもない',
+                          ]"
+                          :key="index"
+                          class="flex items-center gap-2"
+                        >
+                          <RadioGroupItem
+                            :id="`option-${index}-${criteriaIndex}`"
+                            :value="index"
+                            class="cursor-pointer"
+                          />
+                          <Label
+                            :for="`option-${index}-${criteriaIndex}`"
+                            class="cursor-pointer text-xs font-normal"
+                          >
+                            {{ option }}
+                          </Label>
+                        </div>
+                      </RadioGroup>
+                    </div>
                   </div>
                 </div>
 
-                <!-- comment -->
                 <div>
                   <p class="text-sm font-medium">コメント</p>
                   <Textarea
@@ -149,18 +295,6 @@ const hoverStarIndexObj = ref<{
                   </span>
                 </div>
 
-                <!-- anonymous -->
-                <!-- <div name="isAnonymous">
-                  <div class="flex items-center justify-between">
-                    <p class="text-sm text-muted-foreground">
-                      匿名でフィードバックをする
-                    </p>
-                    <Switch
-                      class="cursor-pointer data-[state=checked]:bg-purple"
-                    />
-                  </div>
-                </div> -->
-
                 <Button variant="main" class="w-full cursor-pointer">
                   フィードバックを送信する
                 </Button>
@@ -169,7 +303,7 @@ const hoverStarIndexObj = ref<{
           </Dialog>
           <Button variant="main" class="w-full cursor-pointer" as-child>
             <NuxtLink
-              :to="`/create-form/${template.value}`"
+              :to="`/create-form/${template.id}`"
               class="flex items-center gap-1"
             >
               <Icon name="mdi:plus" />
